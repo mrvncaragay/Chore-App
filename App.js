baba4699cdc4b9334951ea54997c86ee1ae396ef
Marvin Caragay <mrvncaragay@gmail.com>
2020-04-28 14:10:05 -0700

    Refractor project with recent changes

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator, CardStyleInterpolators } from "@react-navigation/stack";
+
 import { Provider } from 'react-redux';
-import { Navigation } from './views';
 import store from './redux/store/store';
 
+import { Login, Landing } from "./screens";
+
+const Home = createStackNavigator();
 
 export default function App() {
+  const [signedIn] = useState(true);
+
+  const HomeStack = () => (
+    <Home.Navigator screenOptions={{
+      headerShown: false,
+      gestureEnabled: true,
+      gestureDirection: "horizontal",
+      cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS
+    }}
+    >
+      <Home.Screen name="Home" component={Landing} />
+      <Home.Screen name="Login" component={Login} />
+    </Home.Navigator>
+  );
+
   return (
     <Provider store={store}>
-      {/* {console.log(store.getState().logins.isLoggedIn)} */}
-      <Navigation />
+      <NavigationContainer>
+        { signedIn ? <>Overview Component</> : <HomeStack />}
+      </NavigationContainer>
     </Provider>
   );
 }
